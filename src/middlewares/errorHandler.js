690f2a45055d6ce20cc04b88d8f51a1c2ab3257b
Juan <juantanuwijaya@gmail.com>
2024-11-07 20:44:28 +0700

    fix: eslint ignore unused vars on errorHandler middlewares

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 import AppError from "../utils/AppError.js";
 
 // Error handler middleware
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => { 
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({ error: err.message });

ec08bfe95d21a025bbe5ebe16089e3402ab64c3e
Juan <juantanuwijaya@gmail.com>
2024-11-07 20:35:21 +0700

    fix: error handler next param

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 import AppError from "../utils/AppError.js";
 
 // Error handler middleware
-const errorHandler = (err, req, res) => { 
+const errorHandler = (err, req, res, next) => { 
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({ error: err.message });
   } else {

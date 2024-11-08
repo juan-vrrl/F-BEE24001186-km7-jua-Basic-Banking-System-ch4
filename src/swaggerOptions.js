c4b3877ee67a40514b88d79b695669d8e257496c
Juan <juantanuwijaya@gmail.com>
2024-11-08 14:41:04 +0700

    update: change main_url to app_url

--- src/swaggerOptions.js
+++ src/swaggerOptions.js
@@ -13,7 +13,7 @@ const options = {
     },
     servers: [
       {
-        url: `${process.env.MAIN_URL}/api/v1`,
+        url: `${process.env.APP_URL}/api/v1`,
       },
     ],
     components: {
